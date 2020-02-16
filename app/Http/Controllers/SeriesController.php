23876832c1b7df60208a86bc4d29c59aaea40c2b
Ricardo Coimbra <coimbra.rico@gmail.com>
2020-02-16 15:38:49 -0400

    Initial commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class SeriesController
+{
+    public function index()
+    {
+        return [
+            "Gray's Anatomy",
+            "Lost"
+        ];
+    }
+
+}
